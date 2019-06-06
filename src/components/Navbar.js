d185c6a52fcf513fbfaedd3bfe8068a2b03fda96
Wilfried Kakou <mianquihin15@gmail.com>
2019-06-06 13:59:44 +0000

    button styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,51 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
+import logo from '../logo.svg'
+import styled from 'styled-components'
 export default class Navbar extends Component {
     render = () => {
         return (
-          <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class=" nav-link text-dark" href="#">Features <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-dark" href="#">Enterprise</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-dark" href="#">Support</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-dark" href="#">Pricing</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-sm-5">
+            <Link to='/'>
+              {/*https://image.flaticon.com/icons/svg/126/126122.svg*/}
+              <img src={logo} alt="my store"  className="navbar-brand"/>
+            </Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <Link to='/' className="nav-link">Products</Link>
+                </li>
+              </ul>
+              <Link to='/cart' className="ml-auto">
+                <ButtonContainer>
+                  <span className="mr-2">
+                    <FontAwesomeIcon icon={faCartPlus} /> my cart
+                  </span>
+                </ButtonContainer>
+              </Link>
+            </div>
+          </nav>
         )
     }
 }
+const ButtonContainer = styled.button`
+text-transform: capitalize;
+font-size:1.4rem;
+background-color:transparent;
+border-radius: 25px;
+padding: 0.2rem 0.5rem;
+cursor: pointer;
+transition: all 0.5s ease-in-out;
+&:hover {
+  background:var(--lightBlue);
+  color:var(--mainBlue)
+}
+&:focus {
+  outline: none
+}
+`;

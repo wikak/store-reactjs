aa5bb25688c24dc0d9d703701ecdbe5dd9638426
Wilfried Kakou <mianquihin15@gmail.com>
2019-06-08 22:27:35 +0000

    add image to product page

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
 import Product from './Product'
-import  {storeProducts} from '../data'
+import { storeProducts } from '../data'
 import Title from './Title'
-
+import { ProductConsumer } from '../context'
 export default class ProductList extends Component {
   state = {
     products: storeProducts
   }
-    render = () => {
-      console.log(this.state.products);
-        return (
-          <React.Fragment>
-            <div className="py-5">
-              <div className="container">
-              <Title name="our" title="products" />
-
-                <div className="row"></div>
-              </div>
+  render = () => {
+    console.log(this.state.products);
+    return (
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+            <div className="row">
+              <ProductConsumer>
+                {value => {
+                  return value.products.map(
+                    product => {
+                      return <Product
+                        key={product.id}
+                        product={product}
+                      />
+                    })
+                }}
+              </ProductConsumer>
             </div>
-          </React.Fragment>
-            //<Product />
-        )
-    }
+          </div>
+        </div>
+      </React.Fragment>
+      //<Product />
+    )
+  }
 }
